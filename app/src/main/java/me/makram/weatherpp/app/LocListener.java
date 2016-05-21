ead274e7ed5ccb2d4147740ac26b907c0f2a89db
Makram Kamaleddine <makram.kd@gmail.com>
2016-05-21 12:04:03 +0400

    add google play services in order to use location services

---
+++ app/src/main/java/me/makram/weatherpp/app/LocListener.java
@@ -0,0 +1,36 @@
+package me.makram.weatherpp.app;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by admin on 5/21/16.
+ */
+public class LocListener implements LocationListener {
+
+
+    public LocListener() {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
--- app/src/main/java/me/makram/weatherpp/app/activities/MainActivity.java
+++ app/src/main/java/me/makram/weatherpp/app/activities/MainActivity.java
@@ -1,20 +1,70 @@
 package me.makram.weatherpp.app.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import me.makram.weatherpp.app.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity {
+import me.makram.weatherpp.app.AppController;
+import me.makram.weatherpp.app.R;
+import me.makram.weatherpp.app.tasks.GetForecastByCoordinatesTask;
+import okhttp3.OkHttpClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
+
+    private static final String TAG = MainActivity.class.getName();
+
+    private GoogleApiClient googleApiClient;
+    private OkHttpClient okHttpClient;
+    private Location lastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buildGoogleApiClient();
+        AppController application = (AppController) getApplication();
+        okHttpClient = application.getClient();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -23,21 +73,26 @@ public class MainActivity extends AppCompatActivity {
 
         switch (id) {
             case R.id.action_refresh:
-                pullWeatherData();
+                if (lastLocation != null) {
+                    new GetForecastByCoordinatesTask(okHttpClient).execute(lastLocation.getLongitude(),
+                            lastLocation.getLatitude());
+                }
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    private void pullWeatherData() {
-
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
     }
 
     /**
@@ -48,5 +103,40 @@ public class MainActivity extends AppCompatActivity {
         ConnectivityManager manager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = manager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    /**
+     * Runs when the GoogleApiClient object successfully connects.
+     * @param connectionHint the connection hint
+     */
+    @Override
+    public void onConnected(@Nullable Bundle connectionHint) {
+        int locationPermission =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (locationPermission == PackageManager.PERMISSION_GRANTED) {
+            lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if (lastLocation != null) {
+
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "Connection suspended. Cause: " + cause);
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed. Reason: " + connectionResult.getErrorMessage());
+    }
+
+    private synchronized void buildGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 }
